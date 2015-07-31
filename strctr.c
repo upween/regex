556315c2aa1fab5f4af0692a7d8c4dd170ecde4f
machangzhe <machangzhe@gmail.com>
2015-07-31 16:56:01 +0800

    Utils can get next char from string or rollback.

---
+++ strctr.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _ctr_string {
+	int idx;
+	char * str;
+} CtrlString;
+
+CtrlString* strctr_init(char * str) {
+	if (str == NULL) {
+		return NULL;
+	}
+
+	CtrlString* string_struct = malloc(sizeof(CtrlString));
+	string_struct -> idx = 0;
+
+	int size = strlen(str);
+	string_struct -> str = malloc(size + 1);
+	strcpy(string_struct -> str, str);
+
+	return string_struct;
+}
+
+void strctr_recycle(CtrlString* str) {
+	free(str -> str);
+	free(str);
+}
+
+char strctr_next(CtrlString* str) {
+	return *(str -> str + str -> idx++);
+}
+
+void strctr_rollback(CtrlString* str) {
+	if (str -> idx > 0) {
+		str -> idx--;
+	}
+}
---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "strctr.c"
+#include "myconst.h"
+
+int main() {
+	char *string = "1234567890";
+	CtrlString* str = strctr_init(string);
+
+	char c;
+	char opt;
+	while (TRUE) {
+		scanf("%c", &opt);
+		if (opt == 'n') {
+			if ((c = strctr_next(str)) != '\0') {
+				printf("%c\n", c);
+			} else {
+				break;
+			}
+		} else if (opt == 'b') {
+			strctr_rollback(str);
+		}
+	}
+
+	strctr_recycle(str);
+	return 0;
+}
