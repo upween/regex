eda9af5b692ce62f70610c5d564b386e64231cbd
machangzhe <machangzhe@gmail.com>
2015-08-07 17:43:46 +0800

    start to add a list struct

--- ilib.c
+++ ilib.c
@@ -17,16 +17,31 @@ int ilib_list_add(List *list, void *node, void index) {
 		return -1;
 	}
 
-	ListNode *list_node = malloc(sizeof(ListNode));
-	if (list_node == NULL) {
+	ListNode *add_node = malloc(sizeof(ListNode));
+	if (add_node == NULL) {
 		return -1;
 	}
 
 	if (index == 0) {
-		
+		add_node -> pre_node = NULL;
+		add_node -> next_node = list -> first_node;
+		list -> first_node -> pre_node = add_node;
+		list -> first_node = add_node;
 	} else if (index == list -> list_size) {
-
+		add_node -> pre_node = list -> last_node;
+		add_node -> next_node = NULL;
+		list -> last_node -> next_node = add_node;
+		list -> last_node = add_node;
 	} else {
-		
+		add_node -> pre_node = list -> first_node;
+		add_node -> next_node = list -> first_node ->next_node;
+		for (int i = 1; i < index; i ++) {
+			add_node -> pre_node = add_node -> next_node;
+			add_node -> next_node = add_node -> next_node -> next_node;
+		}
+		add_node -> pre_node -> next_node = add_node;
+		add_node -> next_node -> pre_node = add_node;
 	}
+	list -> index ++;
+	return list -> index;
 }
