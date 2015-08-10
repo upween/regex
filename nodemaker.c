2645ae9019e380c1d14952de16d61b47179ea9d3
machangzhe <machangzhe@gmail.com>
2015-08-10 19:27:37 +0800

    start to write a nodemaker

--- nodemaker.c
+++ nodemaker.c
@@ -1,3 +1,16 @@
 #include "myconst.h"
 #include "strctr.c"
 
+#define START 0
+#define MIDDLE 1
+#define END 2
+
+typedef struct _nfa_node{
+	int node_type;
+	List *path_list;
+} NNode;
+
+typedef struct _nfa_path{
+	NNode *target_node;
+	List *allowed_char;
+} NPath;
